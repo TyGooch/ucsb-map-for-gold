f0e82ac1cc0421d313c0a2a34adba8414a234a99
Ty Gooch <gooch.ty@gmail.com>
2018-03-12 20:29:59 -0700

    initial commit

---
+++ scripts/changeLink.js
@@ -0,0 +1,5 @@
+var mapLinks = document.querySelectorAll("a[href^='http://map.ucsb.edu']");
+for (var i = 0, l = mapLinks.length; i < l; i++) {
+  mapLink = mapLinks[i];
+  mapLink.href = `https://ucsbmap.com/${mapLink.textContent.split(',')[0].replace(' Building', '').replace(/ /g, '')}`;
+}
